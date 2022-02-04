8f3f769d6c69367110930d3816fe0c033a1446d9
timppeli <91657796+timppeli@users.noreply.github.com>
2022-02-04 11:44:15 +0200

    Added usestates and onChange events

--- src/App.js
+++ src/App.js
@@ -2,21 +2,29 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
+  // Constants
+  const [weight, setWeight] = useState(0);
+  const [bottles, setBottles] = useState(0);
+  const [time, setTime] = useState(0);
+  const [gender, setGender] = useState("gender");
 
+  // Functions
+
+  // Printing the app
   return (
     <div className="container mt-5 mb-3">
       <h1>BAC Calculator</h1>
       <span className="text-muted">Calculating blood alcohol level</span>
       <div className="mt-3 mb-3">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="mb-3">
             <label className="form-label">Weight</label>
-            <input className="form-control" type="number" name="weight"></input>
+            <input className="form-control" type="number" name="weight" value={weight} onChange={e => setWeight(e.target.value)}></input>
             <div className="form-text">Your body weight in kilograms</div>
           </div>
           <div className="mb-3">
             <label className="form-label">Bottles</label>
-            <select className="form-select" name="bottles">
+            <select className="form-select" name="bottles" value={bottles} onChange={e => setBottles(e.target.value)}>
               <option selected value="0">0</option>
               <option value="1">1</option>
               <option value="2">2</option>
@@ -28,7 +36,7 @@ function App() {
           </div>
           <div className="mb-3">
             <label className="form-label">Time</label>
-            <select className="form-select" name="time">
+            <select className="form-select" name="time" value={time} onChange={e => setTime(e.target.value)}>
               <option selected value="0">0</option>
               <option value="1">1</option>
               <option value="2">2</option>
@@ -36,22 +44,22 @@ function App() {
               <option value="4">4</option>
               <option value="5">5</option>
             </select>
-            <div className="form-text">How many hours it has been since your first drink</div>
+            <div className="form-text">How many hours it has been since your</div>
           </div>
           <div className="mb-3">
             <label className="form-label">Gender</label>
             <div class="form-check">
-              <input className="form-check-input" type="radio" name="gender" id="male" value="male"></input>
+              <input className="form-check-input" type="radio" name="gender" id="male" value="male" onChange={e => setGender(e.target.value)}></input>
               <label className="form-label" for="male">Male</label>
             </div>
             <div class="form-check">
-              <input className="form-check-input" type="radio" name="gender" id="female" value="female"></input>
+              <input className="form-check-input" type="radio" name="gender" id="female" value="female" onChange={e => setGender(e.target.value)}></input>
               <label className="form-label" for="female">Female</label>
             </div>
           </div>
           <button className="btn btn-secondary">Calculate</button>
         </form>
-        <div className="alert"></div>
+        <div className="alert">{result}</div>
       </div>
     </div>
   );
